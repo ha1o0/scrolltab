b9d48f2398abccc87cfcbbd01d12a4cf8feb6257
sidney <516202795@qq.com>
2022-01-05 00:43:13 +0800

    init

---
+++ scrolltab/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//
+//  Created by casa on 2020/2/21.
+//  Copyright © 2020 casa. All rights reserved.
+//
+
+import UIKit
+import CTMediator
+
+class ViewController: UIViewController {
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
