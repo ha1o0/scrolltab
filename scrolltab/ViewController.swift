d99cda218f19b15b9f5b1aeebc56fca22c84e509
sidney <516202795@qq.com>
2022-01-06 00:49:45 +0800

    update

--- scrolltab/ViewController.swift
+++ scrolltab/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 import CTMediator
 
 class ViewController: UIViewController {
+    
+    lazy var btn: UIButton = {
+        let _btn = UIButton()
+        _btn.setTitle("test", for: .normal)
+        return _btn
+    }()
+    
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        self.view.addSubview(btn)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        btn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        btn.addTarget(self, action: #selector(to), for: .touchUpInside)
+    }
+    
+    @objc func to() {
+        
     }
 }
