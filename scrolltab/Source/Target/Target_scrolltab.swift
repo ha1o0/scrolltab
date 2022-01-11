6055f41f78aa6a045b9ad01d752a4d781a15cbd7
sidney <516202795@qq.com>
2022-01-12 01:02:11 +0800

    update

--- scrolltab/Source/Target/Target_scrolltab.swift
+++ scrolltab/Source/Target/Target_scrolltab.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 @objc class Target_scrolltab: NSObject {
-    @objc func Action_demo(_ params:[AnyHashable:Any]) -> UIViewController? {
-        guard let name = params["name"] as? String else { return nil }
-        
+    
+    @objc func Action_Extension_ViewController(_ params:NSDictionary) -> UIViewController {
         if let callback = params["callback"] as? (String) -> Void {
-            callback("hello \(name) !")
+            callback("success")
+        }
+
+        let viewController = DemoViewController()
+        return viewController
+    }
+    
+    @objc func Action_Category_ViewController(_ params:NSDictionary) -> UIViewController {
+        
+        if let block = params["callback"] {
+            
+            typealias CallbackType = @convention(block) (NSString) -> Void
+            let blockPtr = UnsafeRawPointer(Unmanaged<AnyObject>.passUnretained(block as AnyObject).toOpaque())
+            let callback = unsafeBitCast(blockPtr, to: CallbackType.self)
+            
+            callback("success")
         }
         
+        let viewController = DemoViewController()
+        return viewController
+    }
+    
+    @objc func Action_viewController(_ params:[AnyHashable:Any]) -> UIViewController? {
+//        guard let name = params["name"] as? String else { return nil }
+//
+//        if let callback = params["callback"] as? (String) -> Void {
+//            callback("hello \(name) !")
+//        }
+
         let viewController = DemoViewController()
         return viewController
     }
